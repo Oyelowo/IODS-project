db9f088a30a2656cb524486d33ffb47f8ddb8586
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2017-11-26 15:16:38 +0200

    wrangled

--- data/assignment4/create_human.R
+++ data/assignment4/create_human.R
@@ -2,7 +2,7 @@
 
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
-
+setwd("C:/Users/oyeda/Desktop/OPEN_DATA_SCIENCE/IODS-project/data/assignment4")
 #column names of the datasets
 colnames(hd)
 colnames(gii)
@@ -39,5 +39,8 @@ gii$eduFM <- gii$edu2F/gii$edu2m
 #create a a new column for the ratio of the female percentage of labour force participation to male
 gii$labFM <- gii$labF / gii$labM
 
-
+#join the two datasets
 human <- inner_join(x=hd, y=gii, by="Country", suffix=c("hd", "gii"))
+
+#save the file
+write.table(human, file = "C:/Users/oyeda/Desktop/OPEN_DATA_SCIENCE/IODS-project/data/assignment4/human.txt", sep = ",", col.names = T)
