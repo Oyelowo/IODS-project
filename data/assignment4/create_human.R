ef31fb52a2be522d572ecbd9567cc42ff6abbb9b
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2017-11-26 15:06:20 +0200

    Data wranlgling done. Interpretation not yet done for analysis

---
+++ data/assignment4/create_human.R
@@ -0,0 +1,43 @@
+#The datasets below are Human Development Index(hd) and Gender Inequality Index(gii)
+
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+
+#column names of the datasets
+colnames(hd)
+colnames(gii)
+
+#Structure of hd
+str(hd)
+
+#dimension
+dim(hd)
+
+#summary
+summary(hd)
+
+#structure of gii
+str(gii)
+
+#dimension
+dim(gii)
+
+#summary
+summary(gii)
+
+
+#change the column names into something shorter and descriptive
+colnames(hd) <-c("HDIrank", "Country", "HDI", "LifeExpBirth","ExpYrsEdu", "MeanYrsEdu",
+                 "GNIperCap", "GNIperCapMinusHDIrank") 
+
+colnames(gii) <- c("GIIrank","Country","GII","MMR","ABR","PRParl", "edu2F","edu2m","labF","labM" )
+
+
+#create new columns for ratio of female secondary education to male
+gii$eduFM <- gii$edu2F/gii$edu2m
+
+#create a a new column for the ratio of the female percentage of labour force participation to male
+gii$labFM <- gii$labF / gii$labM
+
+
+human <- inner_join(x=hd, y=gii, by="Country", suffix=c("hd", "gii"))
